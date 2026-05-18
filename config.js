406827bace81fd840c0ba8d828be74aecd2264d2
ferjosegonza <96452620+ferjosegonza@users.noreply.github.com>
2026-05-18 20:21:58 -0300

    cambio forma de iniciar sesion

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // =====================================================
 const CONFIG = {
     // LA URL DE GOOGLE APPS SCRIPT
-    GAS_URL: "https://script.google.com/macros/s/AKfycbzOkunkwbojRRUACBRM4-BUtUFxmONh0P2i2Nf-5ix2bYTg5PHSMNQJrUZvUT2XCCSh/exec",
+    GAS_URL: "https://script.google.com/macros/s/AKfycbzfOynvdbqIn7xUv00nl9CRwBEKMMSSgK4f_A6Bd4vVjS_2_-TiIrEzLQEolQI-ZjWL/exec",
     
     // Tiempo de expiración de sesión en milisegundos (24 horas)
     SESSION_EXPIRATION: 24 * 60 * 60 * 1000
