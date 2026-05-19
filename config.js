9d17469c785f50869f3e944ef06408a52e3a58c2
ferjosegonza <96452620+ferjosegonza@users.noreply.github.com>
2026-05-19 16:18:17 -0300

    hotfix

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // =====================================================
 const CONFIG = {
     // LA URL DE GOOGLE APPS SCRIPT
-    GAS_URL: "https://script.google.com/macros/s/AKfycbxrDAMWojP5yxSbB83iCcg--FppNYdctccsx6aTsIWNRD9gErCejB0L0Z7BOM6s9Q/exec",
+    GAS_URL: "https://script.google.com/macros/s/AKfycbwZCEyuKlbbJ38gUpcMKftf92h26x8sXB0-RKbY9vQcbSW24zzfZP1aZMFCMg5hJ8BE/exec",
     
     // Tiempo de expiración de sesión en milisegundos (24 horas)
     SESSION_EXPIRATION: 24 * 60 * 60 * 1000
