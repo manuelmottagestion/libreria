4c46f25d2a03d222c64cdd5cbfc1cd1708789e3d
ferjosegonza <96452620+ferjosegonza@users.noreply.github.com>
2026-05-21 18:15:03 -0300

    Refactor dashboard styles and improve error handling for GAS requests

--- config.js
+++ config.js
@@ -3,7 +3,8 @@
 // =====================================================
 const CONFIG = {
     // LA URL DE GOOGLE APPS SCRIPT
-    GAS_URL: "https://script.google.com/macros/s/AKfycby7lJxx9DxYJIDEwlvIUDhG_gEt7giRiUJVlGQLzZhrmMs-PkHsmeokeSM1i43Rhb6K/exec",
+    //GAS_URL: "https://script.google.com/macros/s/AKfycbyIJXpT1FucSUWdSFxSo0VHRWqM8hKrWQZpWCgYMnm2zr18L1BOqjcqmrG14U91a6vO/exec",
+    GAS_URL:"https://script.google.com/macros/s/AKfycby7lJxx9DxYJIDEwlvIUDhG_gEt7giRiUJVlGQLzZhrmMs-PkHsmeokeSM1i43Rhb6K/exec",
     
     // Tiempo de expiración de sesión en milisegundos (24 horas)
     SESSION_EXPIRATION: 24 * 60 * 60 * 1000
